1f02463fe2c678e2705a3632f23a325a1187e816
Kaelen Juntilla <Da4kast@charter.net>
2014-10-14 15:01:36 -0700

    5th push

--- public_html/Js/Snake.js
+++ public_html/Js/Snake.js
@@ -1,11 +1,15 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
 
 function gameInitialize(){
     var canvas = document.getElementById("game-screen");
@@ -25,4 +29,28 @@ function gameLoop() {
 function gameDraw() {
    context.fillStyle = "rgb(129, 133, 25)";
    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++) {
+        snake.push( {
+            x: index,
+            y: 0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    
 }
