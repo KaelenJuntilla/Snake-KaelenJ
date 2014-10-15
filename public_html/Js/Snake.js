f7087543ed98964ea586bd07092901bc470d5d1a
Kaelen Juntilla <Da4kast@charter.net>
2014-10-15 15:02:20 -0700

    6th push

--- public_html/Js/Snake.js
+++ public_html/Js/Snake.js
@@ -8,8 +8,9 @@ var screenHeight;
 
 gameInitialize();
 snakeInitialize();
-gameDraw();
-snakeDraw();
+setInterval(gameLoop, 1000/30);
+
+
 
 function gameInitialize(){
     var canvas = document.getElementById("game-screen");
@@ -23,7 +24,9 @@ function gameInitialize(){
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
 }
 
 function gameDraw() {
@@ -36,7 +39,7 @@ function snakeInitialize() {
     snakeLength = 5;
     snakeSize = 20;
     
-    for(var index = 0; index < snakeLength; index++) {
+    for(var index = snakeLength - 1; index >=0; index--) {
         snake.push( {
             x: index,
             y: 0
@@ -52,5 +55,13 @@ function snakeDraw() {
 }
 
 function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
     
-}
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}    
