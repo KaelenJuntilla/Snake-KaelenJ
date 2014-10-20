6d7c8acc58e21dd88d6d8fa22654d02f21e00238
Kaelen Juntilla <Da4kast@charter.net>
2014-10-20 15:02:42 -0700

    8th push

--- public_html/Js/Snake.js
+++ public_html/Js/Snake.js
@@ -6,6 +6,7 @@
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
 
 var food;
 
@@ -34,6 +35,8 @@ function gameInitialize(){
     
     canvas.width = screenWidth;
     canvas.height = screenHeight;
+    
+    document.addEventListener("keydown", keyboardHandler);
 }
 
 function gameLoop() {
@@ -52,6 +55,7 @@ function snakeInitialize() {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
+    snakeDirection = "down";    
     
     for(var index = snakeLength - 1; index >=0; index--) {
         snake.push( {
@@ -72,7 +76,13 @@ function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    if(snakeDirection == "down") {
+        snakeHeadY++;
+    }
+    else {
+        snakeHeadX++;
+    }
+   
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
@@ -80,14 +90,41 @@ function snakeUpdate() {
     snake.unshift(snakeTail);
 }
 
+/*-----------------------------------------------------------------------------
+ * Food Functions
+ * ----------------------------------------------------------------------------
+ */
+
 function foodInitialize() {
     food = {
         x: 0,
         y: 0
     };
+    setFoodPosition();
 }
 
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect(food.x,  food.y, snakeSize, snakeSize);
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
+    
+    food.x = randomX;
+    food.y = randomY;
+}
+
+/*-----------------------------------------------------------------------------
+ * Input Functions
+ * ----------------------------------------------------------------------------
+ */
+
+function keyboardHandler(event) {
+    console.log(event);
+    
+    if(event.keyCode == "39") {
+        snakeDirection = "Right";
+    }
 }
